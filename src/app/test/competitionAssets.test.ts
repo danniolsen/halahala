cd0e8786539c44f046fdb4f79e86dda427f22392
Danni Olsen <danni.r.olsen@gmail.com>
2025-01-21 14:14:27 +0100

    feeat: test for methods in competitionAssets

---
+++ src/app/test/competitionAssets.test.ts
@@ -0,0 +1,54 @@
+import type { CompetitionType } from "@/src/app/types/Match.type";
+import LaligaLogo from "../../../public/la-liga.svg";
+import ChampionsLogo from "../../../public/champions-league.svg";
+import CopaReyLogo from "../../../public/copa-del-rey.svg";
+import NoLogo from "../../../public/noLogo.svg";
+
+import {
+  competitionMap,
+  getCompetitionColor,
+  getCompetitionLogo,
+  getCompetitionName,
+} from "@/src/app/utils/competitionAssets";
+
+const laLiga: CompetitionType = {
+  name: "LALIGA EA SPORTS",
+};
+
+const ucl: CompetitionType = {
+  name: "UEFA Champions League",
+};
+
+const copaRey: CompetitionType = {
+  name: "Copa del Rey",
+};
+
+const faulty: CompetitionType = {
+  name: "wrong name" as any,
+};
+
+describe("Should return a competitions assets based on competition", () => {
+  test("That the formatted name is returned", () => {
+    // name
+    expect(getCompetitionName(laLiga)).toEqual("La Liga");
+    expect(getCompetitionName(ucl)).toEqual("Champions League");
+    expect(getCompetitionName(copaRey)).toEqual("Copa del Rey");
+    expect(getCompetitionName(faulty)).toEqual("");
+  });
+
+  test("That the correct color is returned based on name", () => {
+    // color
+    expect(getCompetitionColor(laLiga)).toEqual("bg-la-liga");
+    expect(getCompetitionColor(ucl)).toEqual("bg-champions-league");
+    expect(getCompetitionColor(copaRey)).toEqual("bg-copa-del-rey");
+    expect(getCompetitionColor(faulty)).toEqual("bg-grey-400");
+  });
+
+  // logos
+  test("That the correct logo is returned", () => {
+    expect(getCompetitionLogo(laLiga)).toEqual(LaligaLogo);
+    expect(getCompetitionLogo(ucl)).toEqual(ChampionsLogo);
+    expect(getCompetitionLogo(copaRey)).toEqual(CopaReyLogo);
+    expect(getCompetitionLogo(faulty)).toEqual(NoLogo);
+  });
+});
--- src/app/utils/competitionAssets.ts
+++ src/app/utils/competitionAssets.ts
@@ -4,23 +4,20 @@ import ChampionsLogo from "../../../public/champions-league.svg";
 import CopaReyLogo from "../../../public/copa-del-rey.svg";
 import NoLogo from "../../../public/noLogo.svg";
 
-const competitionMap = {
+export const competitionMap = {
   "LALIGA EA SPORTS": {
     logo: LaligaLogo,
     name: "La Liga",
-    //color: "#ff4b44",
     color: "bg-la-liga",
   },
   "UEFA Champions League": {
     logo: ChampionsLogo,
     name: "Champions League",
-    //color: "#0330f7",
     color: "bg-champions-league",
   },
   "Copa del Rey": {
     logo: CopaReyLogo,
     name: "Copa del Rey",
-    // color: "#be052f",
     color: "bg-copa-del-rey",
   },
 };
@@ -30,7 +27,7 @@ const getCompetitionName = (competition: CompetitionType): string => {
 };
 
 const getCompetitionColor = (competition: CompetitionType): string => {
-  return competitionMap[competition?.name]?.color || "";
+  return competitionMap[competition?.name]?.color || "bg-grey-400";
 };
 
 const getCompetitionLogo = (competition: CompetitionType): string => {
