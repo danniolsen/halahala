8546a3ea1f6d4913865bdb4b396a2db184f5e65f
Danni Olsen <danni.r.olsen@gmail.com>
2025-01-27 17:30:10 +0100

    feat: standings list header

---
+++ src/app/components/StandingsHeader.tsx
@@ -0,0 +1,27 @@
+const StandingsHeader = () => {
+  return (
+    <div className="grid grid-cols-9 grid-rows-1 text-slate-500 text-sm text-center">
+      <div className="text-center">
+        <p className="font-semibold">Club</p>
+      </div>
+      <div className="col-span-3"></div>
+      <div className="col-start-5">
+        <p>MP</p>
+      </div>
+      <div className="col-start-6">
+        <p>W</p>
+      </div>
+      <div className="col-start-7">
+        <p>D</p>
+      </div>
+      <div className="col-start-8">
+        <p>L</p>
+      </div>
+      <div className="col-start-9">
+        <p className="font-semibold">Pts</p>
+      </div>
+    </div>
+  );
+};
+
+export default StandingsHeader;
