10fe1d04965cf4b8e48db299d1d99620c7ea0637
Danni Olsen <danni.r.olsen@gmail.com>
2025-01-29 11:51:39 +0100

    feat: remove firebase from project

--- next.config.ts
+++ next.config.ts
@@ -7,11 +7,8 @@ const nextConfig: NextConfig = {
   },
   /* config options here */
   images: {
-    domains: ["assets.laliga.com", "hala-fec0c.firebaseapp.com"],
-    remotePatterns: [
-      { hostname: "assets.laliga.com" },
-      { hostname: "hala-fec0c.firebaseapp.com/" },
-    ],
+    domains: ["assets.laliga.com"],
+    remotePatterns: [{ hostname: "assets.laliga.com" }],
   },
 };
 
