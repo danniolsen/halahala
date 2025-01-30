ebbcfcbee29d251b6f3b16e8ad943c035e50a7df
Danni Olsen <danni.r.olsen@gmail.com>
2025-01-30 17:33:39 +0100

    feat: remove unused components

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -6,7 +6,9 @@ const PageTitle = ({ title }: Props) => {
   return (
     <div className="sticky top-0 z-50 w-full">
       <div className="p-4 bg-background/5 backdrop-blur-lg supports-[backdrop-filter]:bg-background/5">
-        <h2 className="text-3xl font-bold break-words break-all">{title}</h2>
+        <h2 className="text-3xl font-bold break-words break-all text-slate-700">
+          {title}
+        </h2>
       </div>
     </div>
   );
