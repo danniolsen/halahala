3efb34cef8b25498372dabe7f6c97843067bd613
Danni Olsen <danni.r.olsen@gmail.com>
2025-02-07 08:41:18 +0100

    feat: set dark white for matchCard

--- src/components/MatchCard.tsx
+++ src/components/MatchCard.tsx
@@ -22,7 +22,7 @@ const MatchCard = ({
       <div className="px-4 py-2">
         <MatchCardHeader matchDate={date} competition={competition} />
       </div>
-      <div className="bg-white rounded-xl px-4 pt-4">
+      <div className="bg-white dark:bg-slate-800 rounded-xl px-4 pt-4">
         <MatchCardInfo
           homeTeam={home_team}
           homeScore={home_score}
