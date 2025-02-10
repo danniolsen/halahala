ed4a0f832266d5f68816417b7f1bc90136113898
Danni Olsen <danni.r.olsen@gmail.com>
2025-02-10 11:44:16 +0100

    fix: remove fetch dependent component from storybook

--- src/stories/navigation.stories.tsx
+++ src/stories/navigation.stories.tsx
@@ -4,6 +4,14 @@ import Navigation from "@/src/components/Navigation";
 const meta = {
   title: "Navigation",
   component: Navigation,
+  parameters: {
+    nextjs: {
+      appDirectory: true,
+      navigation: {
+        segments: ["upcommingmatches", "standings", "pastmatches", "info"],
+      },
+    },
+  },
 } satisfies Meta<typeof Navigation>;
 
 export default meta;
